a8a95560abf3fcc4f98c9f64dd116b1f0e8c18a1
Fabricio Grela <fgrela@frba.utn.edu.ar>
2026-05-09 21:03:04 -0300

    Agregué la excepción personalizada StockInsuficienteException

---
+++ src/com/techlab/excepciones/StockInsuficienteException.java
@@ -0,0 +1,11 @@
+package com.techlab.excepciones;
+
+public class StockInsuficienteException extends Exception {
+
+    public StockInsuficienteException(String nombreProducto, int stockDisponible, int cantidadPedida) {
+        super(String.format(
+            "Stock insuficiente para '%s'. Disponible: %d, Pedido: %d",
+            nombreProducto, stockDisponible, cantidadPedida
+        ));
+    }
+}
